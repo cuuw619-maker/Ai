1d95775c02d129ba39b20b39f7abd1c4faba20c5
cuuw619-maker <cuuw619@gmail.com>
2026-09-20 11:16:03 +0300

    Apply rustfmt to training module declarations

--- src/training/mod.rs
+++ src/training/mod.rs
@@ -1,14 +1,14 @@
-mod evaluation;
 mod checkpoint;
 mod config;
+mod evaluation;
 mod events;
 mod state;
 mod trainer;
 mod worker;
 
 pub use checkpoint::{Checkpoint, CheckpointData, CheckpointSave};
-pub use evaluation::{Evaluator, TinyEvaluator};
 pub use config::TrainingConfig;
+pub use evaluation::{Evaluator, TinyEvaluator};
 pub use events::{TrainingCommand, TrainingEvent, TrainingProgress};
 pub use state::{TrainingState, TrainingStatus};
 pub use trainer::{next_run_id, Trainer};
