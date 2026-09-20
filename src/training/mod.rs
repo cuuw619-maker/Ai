98dff583beefda0542fd254f3c55ac6402db60ef
cuuw619-maker <cuuw619@gmail.com>
2026-09-20 12:02:55 +0300

    Export live model training snapshots

--- src/training/mod.rs
+++ src/training/mod.rs
@@ -9,7 +9,7 @@ mod worker;
 pub use checkpoint::{Checkpoint, CheckpointData, CheckpointSave};
 pub use config::TrainingConfig;
 pub use evaluation::{Evaluator, TinyEvaluator};
-pub use events::{TrainingCommand, TrainingEvent, TrainingProgress};
+pub use events::{LayerTrainingStats, ModelTrainingSnapshot, TrainingCommand, TrainingEvent, TrainingProgress};
 pub use state::{TrainingState, TrainingStatus};
 pub use trainer::{next_run_id, Trainer};
 pub use worker::TrainingWorker;
